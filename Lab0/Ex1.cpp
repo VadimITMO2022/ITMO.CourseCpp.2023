047f5f8ccdb2a48b7b920c3263d59f14bda0fcb8
VadimITMO2022 <116649038+VadimITMO2022@users.noreply.github.com>
2023-05-21 17:50:56 +0300

    Add files via upload

---
+++ Lab0/Ex1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	// cout << "Hello World!" << endl;
+	cout << "Привет Мир!" << endl;
+	return 0;
+}
