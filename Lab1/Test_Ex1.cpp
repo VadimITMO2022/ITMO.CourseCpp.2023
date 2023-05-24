1009c91da03dd27f580e807972ce8b8ae3f6c5ba
VadimITMO2022 <116649038+VadimITMO2022@users.noreply.github.com>
2023-05-24 16:41:39 +0300

    Add files via upload

---
+++ Lab1/Test_Ex1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include<cmath>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+    cout << "Введите координаты вершин пятиугольника (x,y) \n\n";
+
+    double x1, x2, x3, x4, x5;
+    double y1, y2, y3, y4, y5;
+    double a, b, area;
+
+
+    cout << "Введите координаты 1-ой вершины:\n";
+    cin >> x1 >> y1;
+    cout << "Введите координаты 2-ой вершины:\n";
+    cin >> x2 >> y2;
+    cout << "Введите координаты 3-ей вершины:\n";
+    cin >> x3 >> y3;
+    cout << "Введите координаты 4-ой вершины:\n";
+    cin >> x4 >> y4;
+    cout << "Введите координаты 5-ой вершины:\n";
+    cin >> x5 >> y5;
+
+
+    a = x1 * y2 + x2 * y3 + x3 * y4 + x4 * y5 + x5 * y1;
+    b = x2 * y1 + x3 * y2 + x4 * y3 + x5 * y4 + x1 * y5;
+    area = 0.5 * abs(a - b);
+
+    cout << "Площадь пятиугольника, вычисленная по формуле Гаусса: " << area << endl;
+}
